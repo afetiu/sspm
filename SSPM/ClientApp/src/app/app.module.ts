47f1547dcfaa0cfa0732673bd8699af82d4b71f2
afetiu <afetiu@live.com>
2021-10-27 10:22:48 +0200

    small fix

--- SSPM/ClientApp/src/app/app.module.ts
+++ SSPM/ClientApp/src/app/app.module.ts
@@ -28,23 +28,23 @@ import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ConfirmationService } from 'primeng/api';
 import { NgxLoadingModule } from 'ngx-loading';
 import { ProductlistComponent } from './productlist/productlist.component';
-import {DropdownModule} from 'primeng/dropdown';
+import { DropdownModule } from 'primeng/dropdown';
 import { SupplyComponent } from './supply/supply.component';
-import {AutoCompleteModule} from 'primeng/autocomplete';
-import {StepsModule} from 'primeng/steps';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { StepsModule } from 'primeng/steps';
 import { DatePipe } from './pipes/date.pipe';
-import {InputMaskModule} from 'primeng/inputmask';
-import {CalendarModule} from 'primeng/calendar';
+import { InputMaskModule } from 'primeng/inputmask';
+import { CalendarModule } from 'primeng/calendar';
 import { SaleComponent } from './sale/sale.component';
-import {CheckboxModule} from 'primeng/checkbox';
+import { CheckboxModule } from 'primeng/checkbox';
 import { TransactionlistComponent } from './transactionlist/transactionlist.component';
 import { TransactionTypePipe } from './pipes/transaction-type.pipe';
-import {SpinnerModule} from 'primeng/spinner';
+import { SpinnerModule } from 'primeng/spinner';
 import { TransactedProductsListComponent } from './transacted-products-list/transacted-products-list.component';
-import {OverlayPanelModule} from 'primeng/overlaypanel';
-import { ServicingComponent } from './servicing/servicing.component'; 
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { ServicingComponent } from './servicing/servicing.component';
 import { CommonModule, DecimalPipe } from '@angular/common';
-import {FieldsetModule} from 'primeng/fieldset'; 
+import { FieldsetModule } from 'primeng/fieldset';
 import { FullCalendarModule } from 'ng-fullcalendar';
 
 
@@ -63,12 +63,12 @@ const appRoutes: Routes = [
         { path: 'sale', component: SaleComponent },
         { path: 'transactionlist', component: TransactionlistComponent },
         { path: 'transactedproductlist', component: TransactedProductsListComponent },
-        {path: 'servicinglist', component: ServicingComponent}
+        { path: 'servicinglist', component: ServicingComponent }
 
       ]
   },
   {
-    path: '', redirectTo: '/login', pathMatch: 'full', canActivate: [AuthGuardService]
+    path: '', redirectTo: 'dashboard/stats', pathMatch: 'full', canActivate: [AuthGuardService]
   },
 ];
 
@@ -90,12 +90,12 @@ const appRoutes: Routes = [
     TransactionlistComponent,
     TransactionTypePipe,
     TransactedProductsListComponent,
-    ServicingComponent,  
+    ServicingComponent,
 
 
   ],
   imports: [
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(appRoutes, { useHash: true }),
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
@@ -127,8 +127,8 @@ const appRoutes: Routes = [
     CheckboxModule,
     SpinnerModule,
     OverlayPanelModule,
-    CommonModule, 
-    FieldsetModule, 
+    CommonModule,
+    FieldsetModule,
     FullCalendarModule
 
 
@@ -136,7 +136,7 @@ const appRoutes: Routes = [
   providers: [
     MessageService,
     ConfirmationService,
-  DecimalPipe],
+    DecimalPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
